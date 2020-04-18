bc5703e7d98705b4bdd12fc31a6a785088560315
Faiyaz <55fatehkhan44@gmail.com>
2020-04-18 16:31:14 +0530

    LogIn Example App

---
+++ app/src/main/java/com/m/loginexample/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.m.loginexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
